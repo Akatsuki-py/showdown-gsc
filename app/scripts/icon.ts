d1b6252fbc6afa2227a1b63b3537c3abed787b9a
akatsuki-py <bluejapan73+dev@gmail.com>
2020-04-16 01:56:24 +0900

    #4

--- app/scripts/icon.ts
+++ app/scripts/icon.ts
@@ -1,3 +1,22 @@
+import { NameToID } from './util';
+
+const trimActive = (name: string): string => {
+    return name.replace(' (active)', '');
+};
+
 export const makeIconGen2 = (icon: HTMLSpanElement): HTMLSpanElement => {
+    const ariaLabel = icon.getAttribute('aria-label') || '';
+    let style = icon.getAttribute('style') || '';
+
+    if (ariaLabel == 'Not revealed') {
+        style =
+            'background:transparent url(chrome-extension://fhjacocopnfkoncccpiajpjaghjokimi/images/others/pokeball.png) no-repeat scroll 12px 12px;';
+    } else {
+        const name = trimActive(ariaLabel);
+        const ID = NameToID(name);
+        style = `background:transparent url(chrome-extension://fhjacocopnfkoncccpiajpjaghjokimi/images/minidex/${ID}/0.png) no-repeat scroll 12px 12px;`;
+    }
+    icon.setAttribute('style', style);
+
     return icon;
 };
