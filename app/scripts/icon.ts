8fcf384ef2e6ddcc2bacdde9cca34153a4ed0b02
akatsuki-py <bluejapan73+dev@gmail.com>
2020-04-16 01:59:55 +0900

    #4 IDを動的に

--- app/scripts/icon.ts
+++ app/scripts/icon.ts
@@ -5,16 +5,16 @@ const trimActive = (name: string): string => {
 };
 
 export const makeIconGen2 = (icon: HTMLSpanElement): HTMLSpanElement => {
+    const extensionID = chrome.i18n.getMessage('@@extension_id');
     const ariaLabel = icon.getAttribute('aria-label') || '';
     let style = icon.getAttribute('style') || '';
 
     if (ariaLabel == 'Not revealed') {
-        style =
-            'background:transparent url(chrome-extension://fhjacocopnfkoncccpiajpjaghjokimi/images/others/pokeball.png) no-repeat scroll 12px 12px;';
+        style = `background:transparent url(chrome-extension://${extensionID}/images/others/pokeball.png) no-repeat scroll 12px 12px;`;
     } else {
         const name = trimActive(ariaLabel);
         const ID = NameToID(name);
-        style = `background:transparent url(chrome-extension://fhjacocopnfkoncccpiajpjaghjokimi/images/minidex/${ID}/0.png) no-repeat scroll 12px 12px;`;
+        style = `background:transparent url(chrome-extension://${extensionID}/images/minidex/${ID}/0.png) no-repeat scroll 12px 12px;`;
     }
     icon.setAttribute('style', style);
 
