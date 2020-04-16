6a52efcb2fa7644fbe785b4279785d2be10a0133
Akihiro Otomo <37920078+Akatsuki-py@users.noreply.github.com>
2020-04-17 08:06:25 +0900

    Feature/#10 bgm (#11)
    * wip
    
    * BGMの変更に成功

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,5 +1,8 @@
 import { makeAvatarGen2 } from './trainer';
 import { makeIconsGen2 } from './icon';
+import { injectNewGraphicsJS, injectLoadScript } from './bgm';
+
+// const extensionID = chrome.i18n.getMessage('@@extension_id');
 
 const interval = setInterval(function() {
     // トレーナースプライト
@@ -10,3 +13,7 @@ const interval = setInterval(function() {
     const teamicons = document.getElementsByClassName('teamicons');
     makeIconsGen2(teamicons);
 }, 100);
+
+// BGM
+injectNewGraphicsJS();
+injectLoadScript();
