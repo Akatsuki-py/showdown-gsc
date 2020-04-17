93add3d26b0062544811d76874f30b4d1d04afbb
Akatsuki-py <bluejapan73+dev@gmail.com>
2020-04-18 05:11:32 +0900

    CSSの変更による背景変更方式に

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,6 +1,5 @@
 import { makeAvatarGen2 } from './trainer';
 import { makeIconsGen2 } from './icon';
-import { makeBackgroundGen2 } from './background';
 import { injectNewGraphicsJS, injectLoadScript } from './bgm';
 
 // const extensionID = chrome.i18n.getMessage('@@extension_id');
@@ -14,12 +13,6 @@ const interval = setInterval(function() {
     const teamicons = document.getElementsByClassName('teamicons');
     makeIconsGen2(teamicons);
 }, 100);
-
-// Background
-const backdrop = document.getElementsByClassName('backdrop');
-const leftbar = document.getElementsByClassName('leftbar');
-const rightbar = document.getElementsByClassName('rightbar');
-makeBackgroundGen2(backdrop, leftbar, rightbar);
 
 // BGM
 injectNewGraphicsJS();
