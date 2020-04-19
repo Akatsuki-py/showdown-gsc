d97fd11f6ae5e58d5d24d534c174bfc780cb4ad4
Akatsuki-py <bluejapan73+dev@gmail.com>
2020-04-20 04:42:00 +0900

    webRequest方式に修正

--- app/scripts/inject.ts
+++ app/scripts/inject.ts
@@ -1,15 +1,17 @@
-import { injectScript } from './util';
-
 const extensionID = chrome.i18n.getMessage('@@extension_id');
 const seDir = `chrome-extension://${extensionID}/images/se/`;
-
-export const injectNewBattleJS = () => {
-    injectScript(`chrome-extension://${extensionID}/images/others/battle.js`, 'html');
-};
+const musicDir = `chrome-extension://${extensionID}/images/music/`;
 
 export const injectSEDir = () => {
     const th = document.getElementsByTagName('body')[0];
     const s = document.createElement('script');
     s.textContent = `const seDir = "${seDir}"`;
+    return th.appendChild(s);
+};
+
+export const injectMusicDir = () => {
+    const th = document.getElementsByTagName('body')[0];
+    const s = document.createElement('script');
+    s.textContent = `const musicDir = "${musicDir}"`;
     return th.appendChild(s);
 };
