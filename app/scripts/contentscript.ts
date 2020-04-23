f2eebbeaacd6abcd3d662b4debc816ea3ecc7aa5
Akihiro Otomo <37920078+Akatsuki-py@users.noreply.github.com>
2020-04-23 15:29:58 +0900

    make move-button gen2-style (#46)
    * #5 デザインを金銀風に
    
    * #5 wip
    
    * #5 enable Select SE

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,10 +1,9 @@
 import { makeIconsGen2 } from './icon';
-import { injectSEDir, injectMusicDir, injectIntroDir } from './inject';
+import { injectSEDir, injectMusicDir, injectIntroDir, injectEmbed, injectSelectSE } from './inject';
 
 // const extensionID = chrome.i18n.getMessage('@@extension_id');
 
 const interval = setInterval(function() {
-    // アイコン
     const teamicons = document.getElementsByClassName('teamicons');
     makeIconsGen2(teamicons);
 }, 100);
@@ -13,3 +12,5 @@ const interval = setInterval(function() {
 injectSEDir();
 injectMusicDir();
 injectIntroDir();
+injectEmbed();
+injectSelectSE();
