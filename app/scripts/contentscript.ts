294ac11531d18236cc208b84789994cd00803435
Akatsuki-py <bluejapan73+dev@gmail.com>
2020-04-23 22:22:28 +0900

    fix bug

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -2,17 +2,19 @@ import { makeTeamiconsGen2, makeSwitchIconGen2 } from './icon';
 import { injectSEDir, injectMusicDir, injectIntroDir, injectEmbed, injectSelectSE } from './inject';
 
 // const extensionID = chrome.i18n.getMessage('@@extension_id');
-const switchmenu = document.getElementsByClassName('switchmenu');
-makeSwitchIconGen2(switchmenu);
 
 const interval = setInterval(function() {
     const teamicons = document.getElementsByClassName('teamicons');
     makeTeamiconsGen2(teamicons);
+
+    try {
+        const switchmenu = document.getElementsByClassName('switchmenu');
+        makeSwitchIconGen2(switchmenu);
+    } catch {}
 }, 100);
 
 // SE
 injectSEDir();
 injectMusicDir();
 injectIntroDir();
-injectEmbed();
 injectSelectSE();
