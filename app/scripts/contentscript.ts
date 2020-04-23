2944d7deb714e8afaa93ee4d47c7a37ae78e8307
Akihiro Otomo <37920078+Akatsuki-py@users.noreply.github.com>
2020-04-23 21:39:31 +0900

    make switch-btn gen2-style (#49)
    * wip
    
    * #47

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,11 +1,13 @@
-import { makeIconsGen2 } from './icon';
+import { makeTeamiconsGen2, makeSwitchIconGen2 } from './icon';
 import { injectSEDir, injectMusicDir, injectIntroDir, injectEmbed, injectSelectSE } from './inject';
 
 // const extensionID = chrome.i18n.getMessage('@@extension_id');
+const switchmenu = document.getElementsByClassName('switchmenu');
+makeSwitchIconGen2(switchmenu);
 
 const interval = setInterval(function() {
     const teamicons = document.getElementsByClassName('teamicons');
-    makeIconsGen2(teamicons);
+    makeTeamiconsGen2(teamicons);
 }, 100);
 
 // SE
