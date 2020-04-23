d1d9f42a5ba6d6603e45477490a5be0adf637ca5
Akihiro Otomo <37920078+Akatsuki-py@users.noreply.github.com>
2020-04-24 01:12:35 +0900

    fix README for pulish (#51)
    * wip
    
    * wip
    
    * add gif and features

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,5 +1,5 @@
 import { makeTeamiconsGen2, makeSwitchIconGen2 } from './icon';
-import { injectSEDir, injectMusicDir, injectIntroDir, injectEmbed, injectSelectSE } from './inject';
+import { injectSEDir, injectMusicDir, injectIntroDir, injectSelectSE } from './inject';
 
 // const extensionID = chrome.i18n.getMessage('@@extension_id');
 
@@ -10,7 +10,9 @@ const interval = setInterval(function() {
     try {
         const switchmenu = document.getElementsByClassName('switchmenu');
         makeSwitchIconGen2(switchmenu);
-    } catch {}
+    } catch {
+        // ignore
+    }
 }, 100);
 
 // SE
